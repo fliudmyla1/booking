20640d525aa399d877d43225131e4ba93375b302
fliudmyla1 <93232856+fliudmyla1@users.noreply.github.com>
2022-12-15 21:47:43 +0600

    Add files via upload

---
+++ export1.js
@@ -0,0 +1,7 @@
+module.exports = {
+    amount: 500,
+	emailUser: 'office@weratv.ch',			
+    mongoURI: 'mongodb://127.0.0.1:27017/booking',
+    secretKey: 'warehousebooking',
+    emailPassword: 'XXXXXX'
+}

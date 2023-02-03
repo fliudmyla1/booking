2b692f8e8d0a593a32bd10765fe765781445df24
fliudmyla1 <93232856+fliudmyla1@users.noreply.github.com>
2023-02-03 13:53:37 +0600

    Update export1.js

--- export1.js
+++ export1.js
@@ -1,6 +1,6 @@
 module.exports = {
-    amount: 0,
-	emailUser: '',			
+    amount: 750,
+	emailUser: 'q8singer@gmail.com',			
     mongoURI: 'mongodb://127.0.0.1:27017/booking',
     secretKey: 'warehousebooking',
     emailPassword: 'XXXXXX'

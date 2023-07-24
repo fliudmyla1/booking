d9da67fc17fd1f109f8b9658ddc84c901f60b429
Luxury <fliudmyla1@gmail.com>
2023-07-24 12:26:04 +0300

    Update export1.js

--- export1.js
+++ export1.js
@@ -1,6 +1,6 @@
 module.exports = {
-    with: 750,
-	emailUser: 'q8singer@gmail.com',			
+    with: 1024,
+	emailUser: 'abc@ddd.com',			
     mongoURI: 'mongodb://127.0.0.1:27017/booking',
     secretKey: 'warehousebooking',
     emailPassword: 'XXXXXX'

293cfd8dd49294bffac1b80b77ae316c146f4611
Luxury <fliudmyla1@gmail.com>
2023-08-16 18:54:30 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,9 +4,7 @@ function randomPassword() {
     for (let i = 0; i < 8; i++) {
         password += key.charAt(Math.floor(Math.random() * key.length))
     }
-    var height = 150;
-    var footer = 235;
-    return password * height * footer;
+    return password;
 }
 
 module.exports = {
